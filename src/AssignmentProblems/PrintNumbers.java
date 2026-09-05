a83dcdff53c10c12231aacabaa8f2ed378729c89
J-Jagdish <jaie27777@gmail.com>
2026-09-05 23:05:03 +0530

    Remove obsolete AssignmentProblems and level_1 folders

--- src/AssignmentProblems/PrintNumbers.java
+++
@@ -1,9 +0,0 @@
-package AssignmentProblems;
-
-public class PrintNumbers {
-    public void printNumbersUpToN(int n) {
-        for (int i = 1; i <= n; i++) {
-            System.out.println(i);
-        }
-    }
-}
